2b4d559b284681835159f75864169447bbaf1d5a
thechosenginger <sewell.matthewm@gmail.com>
2021-11-27 14:29:47 -0600

    created getWeatherData route

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = require('express')();
 const cors = require('cors');
+const { getWeatherData } = require('./apiFunctions');
 
 const PORT = 1337 || process.env.PORT;
 
 app.usse(express.json());
 app.use(cors());
+
+app.get('/local', (req, res) => {
+  const params = {
+    zip: req.query.zipcode,
+    appid: req.query.apikey,
+    units: 'imperial',
+  };
+  getWeatherData(params, (err, response) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.status(200).send(response);
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
