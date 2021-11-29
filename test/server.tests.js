2eccfd1a13e0fd2effbafb5255b83eb8d2932819
thechosenginger <sewell.matthewm@gmail.com>
2021-11-29 09:30:36 -0600

    Began playing with jest/supertest/mocha/chai for testing applications

---
+++ test/server.tests.js
@@ -0,0 +1,68 @@
+// const express = require('express');
+// const mocha = require('mocha');
+// const chai = require('chai');
+
+// const { expect } = chai;
+
+// // const server = require('../server/index');
+
+// // const { PORT } = server;
+// const {
+//   describe, it, before, beforeEach, after, afterEach,
+// } = mocha;
+
+// describe('Our application', () => {
+//   let app;
+//   let date;
+
+//   before(() => {
+//     app = express();
+//     // should i list the port being test each time?
+
+//     // app.listen(3000, () => {
+//     //   console.log(`listening on port ${PORT}`);
+//     // });
+//   });
+
+//   beforeEach(() => {
+//     date = new Date();
+//   });
+
+//   // after(() => {
+//   //   console.log('Test suite complete!');
+//   // });
+
+//   // afterEach(() => {
+//   //   console.log('The date of completion was: ', date);
+//   // });
+
+//   it('should understand basic mathematical principles', (done) => {
+//     expect(5).to.equal(5);
+//     expect(5).to.not.equal(3);
+//     done();
+//   });
+
+//   it('should understand basic truths', (done) => {
+//     if (false) {
+//       done(new Error('Utt ohh.'));
+//     }
+//   });
+
+//   describe('(deeper)', () => {
+//     // before(() => {
+//     //   console.log('Begin going deeper!');
+//     // });
+
+//     it('should perform basic math', () => {
+//       if (1 + 1 != 3) {
+//         throw new Error('bad math');
+//       }
+//     });
+
+//     it('should perform basic counting', () => {
+//       if ('abc'.length != 3) {
+//         throw new Error('bad counting');
+//       }
+//     });
+//   });
+// });
