47dd2beee7c7a08776de7aa489aa48cf7358998e
thechosenginger <sewell.matthewm@gmail.com>
2021-11-29 09:31:31 -0600

    added notes

--- server/index.js
+++ server/index.js
@@ -5,8 +5,26 @@ const { getWeatherData } = require('./apiFunctions');
 
 const PORT = 1774 || process.env.PORT;
 
+/**
+ * TODO:
+ *  - is their a db to check valid zip codes
+ *    - could load the below csv file into a db to validate the zip code upon submittal
+ *    - https://www.unitedstateszipcodes.org/zip-code-database/
+ *  - should i add an interface or user input for ease of testing?
+ *  -
+ */
+
 app.use(express.json());
 app.use(cors());
+
+/**
+  127.0.0.1:1774/local?zipcode=786664&apikey=d6e58ff519f14d27b44b1920f9a3830e&units=imperial
+  127.0.0.1:1774/local
+  3 params
+    zipcode
+    apikey
+    units
+ */
 
 app.get('/local', (req, res) => {
   const params = {
@@ -26,3 +44,5 @@ app.get('/local', (req, res) => {
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+
+// module.exports = app;
